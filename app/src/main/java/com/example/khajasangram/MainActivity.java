511baef99803bdcada2b11eeb5386ad268f178e8
igaurab <sushuoit@gmail.com>
2019-07-10 15:27:13 +0545

    Merged layout of Login Activity

--- app/src/main/java/com/example/khajasangram/MainActivity.java
+++ app/src/main/java/com/example/khajasangram/MainActivity.java
@@ -1,24 +1,23 @@
 package com.example.khajasangram;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    EditText email_txt,pw_txt;
+    TextInputEditText email_txt,pw_txt;
     Button login_btn;
     TextView signup_label;
     FirebaseAuth mAuth;
@@ -29,8 +28,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        email_txt = findViewById(R.id.email_login);
-        pw_txt = findViewById(R.id.password_login);
+        email_txt = (TextInputEditText) findViewById(R.id.email_login);
+        pw_txt = (TextInputEditText) findViewById(R.id.password_login);
 
         login_btn = findViewById(R.id.login_btn);
 
